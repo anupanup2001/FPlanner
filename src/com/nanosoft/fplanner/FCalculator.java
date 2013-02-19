d39f95b6866196eeccaaaf8a18aeda8581068419
Anup <anup.anup2001@gmail.com>
2013-02-19 22:11:38 +0530

    Added two functions
    Added comments to functions. Added functions to calculate SIP, CI

--- src/com/nanosoft/fplanner/FCalculator.java
+++ src/com/nanosoft/fplanner/FCalculator.java
@@ -1,16 +1,43 @@
 package com.nanosoft.fplanner;
 
 public class FCalculator {
+	
+	/**
+	 * 
+	 * @param fCurrValue This is the current value whose inflation is to be calculated
+	 * @param fYears Number of years after which inflated value is to be obtained
+	 * @param fInflationRate Inflation rate p.a. Usually it is 6% in India
+	 * @return Inflated value
+	 */
 	public static double calculateInflatedValue(double fCurrValue, double fYears, double fInflationRate) {
 		return fCurrValue * Math.exp(fInflationRate/100.0 * fYears);
 	}
 	
 	/**
-	 * Some people call it Amortization but I prefer to call it EMI :)
-	 * @return
+	 * 
+	 * @param fInterestRate Interest Rate p.a for the loan
+	 * @param fPrincipal Amount borrowed
+	 * @param nMonths Number of months for which it has been borrowed
+	 * @return EMI per month required for loan taken
 	 */
 	public static double calculateEMI(double fInterestRate, double fPrincipal, double nMonths) {
 	    return (fInterestRate/1200.0 * fPrincipal * (Math.pow(1 + fInterestRate/1200.0, nMonths))) / (Math.pow(1 + fInterestRate/1200.0, nMonths) - 1);
+	}
+	
+	/**
+	 * 
+	 * @param fExpectedReturn Future Value of 
+	 * @param fMonths
+	 * @param fInterestRate
+	 * @return Monthly SIP amount required for generating the corpus.
+	 */
+	public static double calculateSIPPerMonth(double fExpectedReturn, double fMonths, double fInterestRate) {
+		return (fExpectedReturn * fInterestRate / 1200.0) / (Math.pow(1 + fInterestRate / 1200.0, fMonths) - 1) * (1 + fInterestRate / 1200.0); 
+	
+	}
+	
+	public static double calculateCompoundInterest(double fPrincipal, double fInterestRate, double fMonths) {
+		return fPrincipal * Math.pow(1 + fInterestRate/1200.0, fMonths);
 	}
 
 }
--- src/com/nanosoft/fplanner/Tester.java
+++ src/com/nanosoft/fplanner/Tester.java
@@ -9,6 +9,8 @@ public class Tester {
 		// TODO Auto-generated method stub
 		System.out.println("Inflation = " + FCalculator.calculateInflatedValue(10000, 1, 6));
 		System.out.println("EMI for loan: " + FCalculator.calculateEMI(10.25, 2290889, 20 * 12));
+		System.out.println("Compound Interest: " + FCalculator.calculateCompoundInterest(10000, 12, 12));
+		System.out.println("SIP required for 10000000: " + FCalculator.calculateSIPPerMonth(10000000, 30 * 12, 10));
 
 	}
 
